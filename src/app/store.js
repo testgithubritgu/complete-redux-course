8a070dcb43881262d0266c6e841c8fa568d99b01
testgithubritgu <swapnilraut1698@gmail.com>
2026-01-30 17:41:00 +0530

    first commit

--- src/app/store.js
+++ src/app/store.js
@@ -8,7 +8,7 @@ export const store = configureStore({
     reducer: {
         counter: counterReducer,
         updateCOunt:counterUpdate,
-        updateName:updateName,
+        updateName:updateName, 
         users:usersReducer
     },
 });
