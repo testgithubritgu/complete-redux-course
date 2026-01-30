dd80abaad798d4d01eb9ee1fd2fc740ee9c50329
testgithubritgu <swapnilraut1698@gmail.com>
2026-01-30 12:33:04 +0530

    state checking

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import counterUpdate from "../features/countUpdate/updateCount";
+import updateName from "../features/namechanger/change-name";
 
 
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
         updateCOunt:counterUpdate,
+        updateName:updateName,
     },
 });
